b8b7f6f3e73c6a8aff7001bd1aca463a39f64968
dulhara <wkldulhara@gmail.com>
2025-03-11 23:00:00 +0530

    model evaluated and r^2 is 0.9

--- linear_regression.py
+++ linear_regression.py
@@ -28,6 +28,7 @@ y = df['Exam Score']  # Output
 #print x which is study hours
 print(x)
 
+#plit your dataset into two parts
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 print( "training X dataset") 
@@ -41,37 +42,39 @@ print(X_test)
 
 print( "testing Y dataset")
 print(y_test)
-# # Training the Model
-# model = LinearRegression()
-# model.fit(X_train, y_train)
 
-# # Model Coefficients
-# print(f"Slope (m): {model.coef_[0]}")
-# print(f"Intercept (c): {model.intercept_}")
+# Training the Model
+model = LinearRegression()
+model.fit(X_train, y_train)
 
-# # Predict on Test Data
-# y_pred = model.predict(X_test)
+# Model Coefficients
+print(f"Slope (m): {model.coef_[0]}")
+print(f"Intercept (c): {model.intercept_}")
 
-# # Compare Predictions
-# results = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-# print(results)
+# Predict on Test Data
+y_pred = model.predict(X_test)
+print(f"predicted values : {y_pred}") 
 
-# # Visualizing Predictions
-# plt.scatter(X_train, y_train, color='blue', label='Training Data')
-# plt.scatter(X_test, y_test, color='red', label='Actual Test Data')
-# plt.plot(X_test, y_pred, color='green', linewidth=2, label='Regression Line')
-# plt.xlabel("Study Hours")
-# plt.ylabel("Exam Score")
-# plt.title("Linear Regression: Study Hours vs Exam Score")
-# plt.legend()
-# plt.show()
+# Compare Predictions
+results = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+print(results)
 
-# # Model Evaluation
-# mse = mean_squared_error(y_test, y_pred)
-# r2 = r2_score(y_test, y_pred)
+# Visualizing Predictions
+plt.scatter(X_train, y_train, color='blue', label='Training Data')
+plt.scatter(X_test, y_test, color='red', label='Actual Test Data')
+plt.plot(X_test, y_pred, color='green', linewidth=2, label='Regression Line')
+plt.xlabel("Study Hours")
+plt.ylabel("Exam Score")
+plt.title("Linear Regression: Study Hours vs Exam Score")
+plt.legend()
+plt.show()
 
-# print(f"Mean Squared Error: {mse}")
-# print(f"R² Score: {r2}")
+# Model Evaluation
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error: {mse}")
+print(f"R² Score: {r2}")
 
 # # Predict for a new student who studies 7.5 hours
 # new_study_hours = np.array([[7.5]])
